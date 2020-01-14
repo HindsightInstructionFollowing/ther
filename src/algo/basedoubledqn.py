correcting gamma computation in replay buffer

src/algo/basedoubledqn.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 
 from algo.neural_architecture import MinigridConvPolicy, MlpNet, MinigridRecurrentPolicy
 from algo.replay_buffer import ReplayMemory, RecurrentReplayBuffer
-from algo.learnt_her import LearntHindsightExperienceReplay
+from algo.learnt_her import LearntHindsightExperienceReplay, LearntHindsightRecurrentExperienceReplay
 
 import time
 
@@ -47,17 +47,28 @@ class BaseDoubleDQN(nn.Module):
         if config["experience_replay_config"]["use_ther"]:
             input_shape = env.observation_space["image"].shape[-3:] # todo : input shape check, because of framestacking etc...
             vocabulary_size = int(env.observation_space["mission"].high.max())
-            replay_buffer = LearntHindsightExperienceReplay(input_shape=input_shape,
-                                                            n_output=vocabulary_size,
-                                                            config=config["experience_replay_config"],
-                                                            device=device,
-                                                            logger=logger
-                                                            )
 
-        elif config["architecture"] == "conv_lstm":
-            replay_buffer = RecurrentReplayBuffer(config=config["experience_replay_config"])
+            if config["architecture"] == "conv_lstm":
+                replay_buffer = LearntHindsightRecurrentExperienceReplay(input_shape=input_shape,
+                                                                         n_output=vocabulary_size,
+                                                                         config=config["experience_replay_config"],
+                                                                         device=device,
+                                                                         logger=logger
+                                                                         )
+            else:
+                replay_buffer = LearntHindsightExperienceReplay(input_shape=input_shape,
+                                                                n_output=vocabulary_size,
+                                                                config=config["experience_replay_config"],
+                                                                device=device,
+                                                                logger=logger
+                                                                )
+
+
         else:
-            replay_buffer = ReplayMemory(config=config["experience_replay_config"])
+            if config["architecture"] == "conv_lstm":
+                replay_buffer = RecurrentReplayBuffer(config=config["experience_replay_config"])
+            else:
+                replay_buffer = ReplayMemory(config=config["experience_replay_config"])
 
         self.replay_buffer = replay_buffer
 
@@ -128,7 +139,7 @@ class BaseDoubleDQN(nn.Module):
         batch_terminal = torch.as_tensor(batch_transitions.terminal, dtype=torch.int32, device=self.device)
         batch_action = torch.as_tensor(batch_transitions.action, dtype=torch.long, device=self.device).reshape(-1, 1)
         batch_mission_length = torch.cat(batch_transitions.mission_length).to(self.device)
-        batch_gamma = torch.cat(batch_transitions.gamma).to(self.device) # For n-step gamma might vary a bit
+        batch_gamma = torch.FloatTensor(batch_transitions.gamma).to(self.device) # For n-step gamma might vary a bit
 
         batch_mission = nn.utils.rnn.pad_sequence(sequences=batch_transitions.mission,
                                                   batch_first=True,
@@ -147,13 +158,16 @@ class BaseDoubleDQN(nn.Module):
 
         # Double DQN
         if torch.sum(batch_terminal) != self.batch_size:
+            terminal_index = batch_terminal == 0
+            assert targets[terminal_index].sum() == 0
+
             # Selection of the action with the policy net
             q_values, _, _ = self.policy_net(batch_next_state_non_terminal_dict)
             q_values_next_state, _, _ = self.target_net(batch_next_state_non_terminal_dict)
 
             args_actions = q_values.max(1)[1].reshape(-1, 1)
-            targets[batch_terminal == 0] = targets[batch_terminal == 0] \
-                                       + batch_gamma * q_values_next_state.gather(1, args_actions).detach()
+            targets[terminal_index,0] = targets[terminal_index].view(-1) \
+                                       + batch_gamma[terminal_index] * q_values_next_state.gather(1, args_actions).view(-1).detach()
 
         targets = targets.reshape(-1)
 

src/algo/learnt_her.py
@@ -1,4 +1,4 @@
-from algo.replay_buffer import AbstractReplay
+from algo.replay_buffer import AbstractReplay, RecurrentReplayBuffer
 from algo.neural_architecture import InstructionGenerator, compute_accuracy
 import numpy as np
 import torch
@@ -24,6 +24,8 @@ class LearntHindsightExperienceReplay(AbstractReplay):
         self.n_sample_before_using_generator = config["n_sample_before_using_generator"]
         self.batch_size =                      config["batch_size"]
         self.accuracy_convergence =            config["accuracy_convergence"] # Accuracy before generator is considered good
+        self.max_update_generator_per_epochs = config["max_update_per_epochs"]
+
         self.padding_value =                   2 # By convention, checked by Word2Idx in wrappers.py
         self.n_update_generator =              0 # Number of optim steps done on generator
         self.generator_usage =                 0 # Number of time a sequence has been relabeled
@@ -56,7 +58,8 @@ class LearntHindsightExperienceReplay(AbstractReplay):
         # ==========================================================================================
 
         # Saving everything except reward and next_state
-        self.last_transitions.append((current_state, action, terminal, mission, mission_length))
+        self.last_transitions.append((current_state, action, mission, mission_length))
+        self.last_terminal.append(terminal)
         self.last_returns.append(reward)
         self.last_states.append(next_state)
 
@@ -66,17 +69,18 @@ class LearntHindsightExperienceReplay(AbstractReplay):
         if len(self.last_states) >= self.n_step:
             self.add_n_step_transition(self.n_step)
 
-            # If terminal, add all sample even if n_step is not available
-            if terminal:
-                truncate_n_step = 1
-                while len(self.last_transitions) > 0:
-                    current_n_step = self.n_step - truncate_n_step
-                    self.add_n_step_transition(n_step=current_n_step)
-                    truncate_n_step += 1
+        # If terminal, add all sample even if n_step is not available
+        if terminal:
+            truncate_n_step = 1
+            while len(self.last_transitions) > 0:
+                current_n_step = self.n_step - truncate_n_step
+                self.add_n_step_transition(n_step=current_n_step)
+                truncate_n_step += 1
 
-                assert len(self.last_returns) == 0
-                assert len(self.last_transitions) == 0
-                self.last_states = []
+            assert len(self.last_returns) == 0
+            assert len(self.last_transitions) == 0
+            assert len(self.last_terminal) == 0
+            self.last_states = []
 
         # ==================== Deal with generator training and dataset ============================
         # ==========================================================================================
@@ -105,7 +109,7 @@ class LearntHindsightExperienceReplay(AbstractReplay):
                     n_correct_attrib = self._cheat_check(true_mission=hindsight_mission,
                                                          generated_mission=generated_hindsight_mission)
 
-                    self.logger.log("gen/n_correct_attrib_gen", n_correct_attrib, self.generator_usage)
+                    self.logger.log("gen/n_correct_attrib_gen", n_correct_attrib)
                     self.generator_usage += 1
 
                 # Substitute the old mission with the new one, change the reward at the end of episode
@@ -129,6 +133,7 @@ class LearntHindsightExperienceReplay(AbstractReplay):
 
     def _train_generator(self):
 
+        gen_update_this_epoch = 1
         convergence = False
         accuracies = []
         len_dataset = len(self.generator_dataset["states"])
@@ -193,15 +198,43 @@ class LearntHindsightExperienceReplay(AbstractReplay):
             loss.backward()
             self.optimizer.step()
 
-            if self.logger:
+            if self.logger and gen_update_this_epoch % 1000 == 0:
                 self.logger.add_scalar("gen/generator_loss", loss.detach().item(), self.n_update_generator)
                 self.logger.add_scalar("gen/generator_accuracy", accuracy, self.n_update_generator)
 
             self.n_update_generator += 1
-            if np.mean(accuracies[-10:]) > self.accuracy_convergence:
+            gen_update_this_epoch += 1
+            del loss
+
+            if np.mean(accuracies[-10:]) > self.accuracy_convergence or gen_update_this_epoch > self.max_update_generator_per_epochs:
                 convergence = True
 
         print("Done training generator in {} steps\nLast accuracies : {}".format(self.n_update_generator, accuracies[-10:]))
+
+
+class LearntHindsightRecurrentExperienceReplay(LearntHindsightExperienceReplay):
+
+    def __init__(self, input_shape, n_output, config, device, logger=None):
+
+        self.len_sum = 0
+        super().__init__(input_shape=input_shape, n_output=n_output, config=config, device=device, logger=logger)
+
+        self.episode_length = np.zeros(self.memory_size)
+        self.transition_proba = np.zeros(self.memory_size)
+        self.last_position = 0
+
+
+    def _store_episode(self, episode_to_store):
+        RecurrentReplayBuffer._store_episode(self, episode_to_store)
+
+    def sample(self, batch_size):
+        return RecurrentReplayBuffer.sample(self, batch_size)
+
+    def __len__(self):
+        return int(self.len_sum)
+
+
+
 
 
 if __name__ == "__main__":

src/algo/replay_buffer.py
@@ -36,7 +36,7 @@ class AbstractReplay(ABC):
         self.last_transitions = [] # Logging rest of the transitions (current_state, action etc ...)
 
         self.gamma = config["gamma"]
-        self.gamma_array = np.array([self.gamma**i for i in range(self.n_step)])
+        self.gamma_array = np.array([self.gamma**i for i in range(self.n_step+1)])
 
     def sample(self, batch_size):
         return random.sample(self.memory[:self.n_episodes], batch_size)
@@ -58,14 +58,21 @@ class AbstractReplay(ABC):
     def add_n_step_transition(self, n_step):
         current_state, action, mission, mission_length = self.last_transitions[0]
 
+        # Not enough samples to apply full n-step, aborting this step
+        if len(self.last_returns) < n_step:
+            return
+
         sum_return = np.sum(self.last_returns[:n_step] * self.gamma_array[:n_step])
         n_step_state = self.last_states[n_step - 1]
-        terminal = self.last_terminal[n_step -1]
+        last_terminal = self.last_terminal[n_step -1]
+
+        if not last_terminal:
+            assert sum_return == 0
 
         self.current_episode.append(
             self.transition(current_state=current_state, action=action, reward=sum_return, next_state=n_step_state,
-                            terminal=terminal, mission=mission, mission_length=mission_length,
-                            gamma=self.gamma_array[n_step-1]**n_step)
+                            terminal=last_terminal, mission=mission, mission_length=mission_length,
+                            gamma=self.gamma_array[n_step])
         )
 
         # Clean
@@ -114,18 +121,18 @@ class ReplayMemory(AbstractReplay):
         if len(self.last_states) >= self.n_step:
             self.add_n_step_transition(self.n_step)
 
-            # If terminal, add all sample even if n_step is not available
-            if terminal:
-                truncate_n_step = 1
-                while len(self.last_transitions) > 0:
-                    current_n_step = self.n_step - truncate_n_step
-                    self.add_n_step_transition(n_step = current_n_step)
-                    truncate_n_step += 1
+        # If terminal, add all sample even if n_step is not available
+        if terminal:
+            truncate_n_step = 1
+            while len(self.last_transitions) > 0:
+                current_n_step = self.n_step - truncate_n_step
+                self.add_n_step_transition(n_step = current_n_step)
+                truncate_n_step += 1
 
-                assert len(self.last_terminal) == 0
-                assert len(self.last_returns) == 0
-                assert len(self.last_transitions) == 0
-                self.last_states = []
+            assert len(self.last_terminal) == 0
+            assert len(self.last_returns) == 0
+            assert len(self.last_transitions) == 0
+            self.last_states = []
 
         # ============= Apply Hinsight Experience Replay by swapping a mission =====================
         # ==========================================================================================