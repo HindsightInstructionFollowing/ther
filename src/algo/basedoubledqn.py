Correcting prioritize calculation and is_weight with distributed buffe

src/algo/basedoubledqn.py
@@ -7,6 +7,8 @@ import torch.nn.functional as F
 from algo.neural_architecture import MinigridConvPolicy, MlpNet, MinigridRecurrentPolicy
 from algo.replay_buffer_parallel import ReplayBufferParallel
 
+from algo.learnt_her import LearntHindsightExperienceReplay
+
 from algo.transition import basic_transition
 
 import time
@@ -33,9 +35,17 @@ class BaseDoubleDQN(nn.Module):
         self.target_net.load_state_dict(self.policy_net.state_dict())
         self.target_net.eval()
 
-        self.optimizer = torch.optim.RMSprop(self.policy_net.parameters(),
-                                             lr=config["lr"],
-                                             weight_decay=config["weight_decay"])
+        self.optimize = config["optimize"] if "optimize" in config else True
+        if config["optimizer"].lower() == 'rmsprop':
+            self.optimizer = torch.optim.RMSprop(self.policy_net.parameters(),
+                                                 lr=config["lr"],
+                                                 weight_decay=config["weight_decay"])
+        elif config["optimizer"].lower() == 'adam':
+            self.optimizer = torch.optim.Adam(self.policy_net.parameters(),
+                                              lr=config["lr"],
+                                              weight_decay=config["weight_decay"])
+        else:
+            raise NotImplementedError("Optimizer '{}' doesn't exist".format(config["optimizer"].lower()))
 
         self.n_optimize_per_step = config["n_optimize_per_step"]
         self.n_update_policy_net = 0
@@ -51,12 +61,12 @@ class BaseDoubleDQN(nn.Module):
 
         # Create replay buffer here
         if config["experience_replay_config"]["use_ther"]:
-            replay_buffer = LearntHindsightRecurrentExperienceReplay(config=config["experience_replay_config"],
-                                                                     is_recurrent=is_recurrent,
-                                                                     env=env,
-                                                                     device=device,
-                                                                     logger=logger
-                                                                     )
+            replay_buffer = LearntHindsightExperienceReplay(config=config["experience_replay_config"],
+                                                            is_recurrent=is_recurrent,
+                                                            env=env,
+                                                            device=device,
+                                                            logger=logger
+                                                            )
         else:
             replay_buffer = ReplayBufferParallel(config=config["experience_replay_config"],
                                                  is_recurrent=is_recurrent,

src/algo/learnt_her.py
@@ -4,9 +4,12 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import pickle as pkl
+from os import path
 
 from nltk.translate import bleu_score
 import time
+
+from algo.transition import basic_transition
 
 class LearntHindsightExperienceReplay(ReplayBufferParallel):
     def __init__(self, config, is_recurrent, env = None, recurrent_memory_saving = 10, logger=None, device=None):
@@ -101,10 +104,13 @@ class LearntHindsightExperienceReplay(ReplayBufferParallel):
             self.logger.log("gen/len_dataset", len(self.generator_dataset["states"]))
         # Number of sample in the generator dataset
         n_generator_example = len(self.generator_dataset['states'])
+
         # If there are enough training example, train the generator
         if n_generator_example > self.generator_next_update:
             self._train_generator()
             self.generator_next_update += self.update_steps
+            generator_dataset_path = path.join(self.logger.path_to_log, "generator_dataset.pkl")
+            pkl.dump(self.generator_dataset, open(generator_dataset_path, "wb"))
 
         # ============= Apply Hinsight Experience Replay by swapping a mission =====================
         # ==========================================================================================
@@ -115,10 +121,10 @@ class LearntHindsightExperienceReplay(ReplayBufferParallel):
                 last_states =     self.current_episode[-self.n_state_to_predict_instruction:]
                 trajectory =      [self.compressor.decompress_elem(t.current_state) for t in last_states]
                 if len(trajectory) < self.n_state_to_predict_instruction:
-                    trajectory =      [self.dummy_state] * (self.n_state_to_predict_instruction - len(trajectory)) + trajectory
-                trajectory =      torch.cat(trajectory, dim=0).to(self.device)
+                    trajectory =  [self.dummy_state] * (self.n_state_to_predict_instruction - len(trajectory)) + trajectory
+                trajectory =      torch.FloatTensor(np.concatenate(trajectory, axis=0)).to(self.device)
 
-                generated_hindsight_mission = self.generate(trajectory)
+                generated_hindsight_mission, generated_hindsight_mission_len = self.generate(trajectory)
 
                 if trajectory.size(2) == 7: # Check only for minigrid (vizdoom instruction are different)
                     n_correct_attrib = self._cheat_check(true_mission=hindsight_mission,
@@ -149,9 +155,9 @@ class LearntHindsightExperienceReplay(ReplayBufferParallel):
                         hindsight_reward = 1 * self.gamma ** (len(self.current_episode) - step - 1)
                     else:
                         hindsight_reward = 0
-                    len_mission = torch.LongTensor([len(generated_hindsight_mission)])
+                    len_mission = torch.LongTensor([generated_hindsight_mission_len])
                     hindsight_episode.append(
-                        self.transition(st, a, hindsight_reward, st_plus1, end_ep, generated_hindsight_mission, len_mission, gamma)
+                        basic_transition(st, a, hindsight_reward, st_plus1, end_ep, generated_hindsight_mission, len_mission, gamma)
                     )
                 self._store_episode(hindsight_episode)
 
@@ -164,12 +170,10 @@ class LearntHindsightExperienceReplay(ReplayBufferParallel):
                 if len_traj < self.n_state_to_predict_instruction:
                     trajectory_to_predict = [self.dummy_state] * (self.n_state_to_predict_instruction - len_traj) + trajectory_to_predict
 
-                self.generator_dataset["states"].append(torch.cat(trajectory_to_predict, dim=0).unsqueeze(0))
+                self.generator_dataset["states"].append(np.expand_dims(np.concatenate(trajectory_to_predict, axis=0), axis=0))
                 self.generator_dataset["instructions"].append(mission)
                 self.generator_dataset["lengths"].append(mission.size(0))
                 self.generator_dataset["correct_obj_name"].append(correct_obj_name)
-                # if len(self.generator_dataset["states"]) % 10 == 0:
-                #     pkl.dump(self.generator_dataset, open("saved_tools/generator_dataset7.pkl", "wb"))
 
             self._store_episode(self.current_episode)
             self.current_episode = []
@@ -184,9 +188,9 @@ class LearntHindsightExperienceReplay(ReplayBufferParallel):
         states = self.generator_dataset['states']
 
         states, instructions, lengths = zip(*sorted(zip(states, instructions, lengths),
-                                            key=lambda x: -x[0].size(0)))
+                                            key=lambda x: -x[0].shape[0]))
 
-        states = torch.cat(states, dim=0)
+        states = torch.FloatTensor(np.concatenate(states, axis=0))
 
         self.mean_norm = states.mean(dim=(0, 1))
         self.std_norm = states.std(dim=(0, 1))

src/algo/neural_architecture.py
@@ -57,7 +57,7 @@ def conv_factory(input_shape, channels, kernels, strides, max_pool, batch_norm):
                             )
 
         if batch_norm[layer]:
-            conv_net.add_module('batch_norm'.format(layer),
+            conv_net.add_module('batch_norm{}'.format(layer),
                                 module=nn.BatchNorm2d(channels[layer]))
 
         conv_net.add_module(name='relu{}'.format(layer),
@@ -154,11 +154,6 @@ class MinigridRecurrentPolicy(nn.Module):
 
         # Adding memory to the agent
         self.memory_rnn = nn.LSTM(input_size=self.size_after_text_viz_merge, hidden_size=self.memory_size, batch_first=True)
-        self.use_layer_norm = config["use_layer_norm"]
-        if self.use_layer_norm:
-            # layer norm creation : 2 norms because lstm splits hidden into hx and cx
-            self.layer_norm_hx = nn.LayerNorm(normalized_shape=[self.memory_size])
-            self.layer_norm_cx = nn.LayerNorm(normalized_shape=[self.memory_size])
 
         self.critic = nn.Sequential(
             nn.Linear(self.memory_size, self.last_hidden_fc_size),
@@ -237,12 +232,6 @@ class MinigridRecurrentPolicy(nn.Module):
                                                                                   enforce_sorted=False)
 
         hidden_memory = (ht[:, :, :self.memory_size], ht[:, :, self.memory_size:])
-
-        if self.use_layer_norm:
-            hidden_memory = (
-                self.layer_norm_hx(hidden_memory[0]),
-                self.layer_norm_cx(hidden_memory[1])
-            )
 
         all_ht, hidden_memory = self.memory_rnn(vision_and_text_sequence_format, hidden_memory)
         all_ht, size = torch.nn.utils.rnn.pad_packed_sequence(all_ht, batch_first=True)
@@ -442,6 +431,8 @@ class InstructionGenerator(nn.Module):
         # This is a convention, veryfied by the environment tokenizer, might be ugly
         self.BEGIN_TOKEN = 0
         self.END_TOKEN = 1
+        self.PAD_TOKEN = 2
+
 
         self.vocabulary_size = n_output
         self.generator_max_len = config["generator_max_len"]
@@ -556,6 +547,11 @@ class InstructionGenerator(nn.Module):
 
         return logits
 
+    def pad(self, tokens):
+        len_seq = len(tokens)
+        tokens += [self.PAD_TOKEN] * (self.generator_max_len - len_seq)
+        return tokens, len_seq
+
     def _generate(self, states_seq):
 
         states_seq = states_seq[-self.n_state_to_predict_instruction:]
@@ -590,7 +586,9 @@ class InstructionGenerator(nn.Module):
 
                 next_token = next_token.unsqueeze(0)
 
-        return torch.LongTensor(generated_token)
+
+        padded_mission, length_mission = self.pad(generated_token)
+        return torch.LongTensor(padded_mission), length_mission
 
 
 

src/algo/recurrent_dqn.py
@@ -3,10 +3,9 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-from algo.transition import basic_transition
-
 import random
-import copy
+from os import path
+import numpy as np
 
 class RecurrentDQN(BaseDoubleDQN):
     def __init__(self, env, config, logger=None, visualizer=None, test_env=None, device='cpu'):
@@ -73,7 +72,7 @@ class RecurrentDQN(BaseDoubleDQN):
         batch_last_action =     batch_dict["last_action"].to(device=self.device)
         batch_gamma =           batch_dict["gamma"].type(torch.FloatTensor).view(-1)[batch_mask == 1].view(-1, 1).to(device=self.device)
 
-        is_weights =            torch.FloatTensor(is_weights).to(self.device)
+        is_weights =            torch.FloatTensor([is_weights]).to(self.device)
 
         #============= Computing targets ===========
         #===========================================
@@ -122,29 +121,31 @@ class RecurrentDQN(BaseDoubleDQN):
 
         # Update prio
         delta = torch.abs(predictions - targets).detach().cpu().numpy()
-        self.replay_buffer.update_transitions_proba(delta, batch_dict["id_retrieved"][0])
+        self.replay_buffer.update_transitions_proba(errors=delta,
+                                                    id_retrieved=batch_dict["id_retrieved"][0],
+                                                    verification_id=batch_dict["id_verification"][0])
 
         # Loss
         loss = F.smooth_l1_loss(predictions, targets, reduction='none') * is_weights
         loss = loss.mean()
-        # Optimization
-        self.optimizer.zero_grad()
-        loss.backward()
 
-        # Keep the gradient between (-1,1). Works like one uses L1 loss for large gradients (see Huber loss)
-        for name, param in self.policy_net.named_parameters():
-            if hasattr(param.grad, 'data'):
-                param.grad.data.clamp_(-1, 1)
-
-        # self.old_parameters = dict()
-        # for k, v in self.target_net.state_dict().items():
-        #     self.old_parameters[k] = v.cpu()
-
-        # Do the gradient descent step
-        self.optimizer.step()
+        if self.optimize:
+            # Optimization
+            self.optimizer.zero_grad()
+            loss.backward()
+            grad_norm = nn.utils.clip_grad_norm_(self.policy_net.parameters(), self.grad_norm_limit)
+            # Do the gradient descent step
+            self.optimizer.step()
 
         if self.environment_step % self.update_target_every == 0:
             self.target_net.load_state_dict(self.policy_net.state_dict())
+            path_weight = path.join(self.writer.path_to_log, "weights.th")
+            torch.save(self.policy_net.state_dict(), path_weight)
+
+            if hasattr(self.replay_buffer.batch_sampler, 'prioritize_p'):
+                prioritize_path = path.join(self.writer.path_to_log, "prioritize_p.npy")
+                np.save(prioritize_path, self.replay_buffer.batch_sampler.prioritize_p)
+
             self.n_update_target += 1
 
         # self.new_parameters = dict()
@@ -155,8 +156,9 @@ class RecurrentDQN(BaseDoubleDQN):
         # Log important info, see logging_helper => SweetLogger for more details
         if self.writer:
             self.writer.store_buffer_id(batch_dict["id_retrieved"][0])
-            self.writer.log("train/percent_terminal", batch_terminal.sum().item() / self.batch_size)
             self.writer.log("train/n_update_target", self.n_update_target)
+            if self.optimize:
+                self.writer.log("train/grad_norm", grad_norm, ['max', 'mean'])
 
         return loss.detach().item()
 

src/algo/replay_buffer_parallel.py
@@ -13,7 +13,6 @@ from algo.transition import basic_transition
 from algo.replay_buffer_tools import WeightedSampler, MemoryDatasetRecurrent, MemoryDataset
 
 
-
 from torch._six import container_abcs, string_classes, int_classes
 numpy_type_map = {
     'float64': torch.DoubleTensor,
@@ -138,16 +137,22 @@ class ReplayBufferParallel(DataLoader):
                          num_workers=config["num_workers"],
                          batch_sampler=weighted_sampler,
                          collate_fn=default_collate,
+                         #worker_init_fn=custom_test,
                          pin_memory=True)
 
         self.iter = None
+        self.num_iter_call = 0
+        self.reset_every = 200
 
     def sample(self):
-        if self.iter is None:
+
+        if self.iter is None or self.num_iter_call % self.reset_every == 0:
             self.iter = self.__iter__()
-        self.batch_sampler.compute_prioritize_proba()
+            self.num_iter_call = 0
+
         batch = next(self.iter)
-        return batch, self.compute_is_weights(batch["id_retrieved"][0])
+        self.num_iter_call += 1
+        return batch, self.compute_is_weights(batch["id_retrieved"][0], batch["id_verification"][0], batch["state_sequence_lengths"])
 
     def _store_episode(self, episode_to_store):
         old_position, new_position, new_n_memory_cell = self.dataset.store_episode(episode_to_store)
@@ -157,7 +162,7 @@ class ReplayBufferParallel(DataLoader):
 
         assert self.batch_sampler.n_memory_cell == self.dataset.n_memory_cell
 
-    def update_transitions_proba(self, errors, id_retrieved):
+    def update_transitions_proba(self, errors, id_retrieved, verification_id):
         """
         Two Cases needed here because the way the error are computed between recurrent and normal replay buffer
 
@@ -177,27 +182,51 @@ class ReplayBufferParallel(DataLoader):
                 processed_errors = []
                 last_l = 0
                 errors = np.power(errors, self.prioritize_alpha)
-                for l in self.dataset.episode_length[id_retrieved]:
-                    l = int(l)
-                    err_list = errors[last_l:last_l + l]
+                id_to_remove = []
+                for num_l, length in enumerate(self.dataset.episode_length[id_retrieved]):
+                    if self.dataset.episode_identificator[id_retrieved][num_l] != verification_id[num_l]: # Out-dated batch sample not in replay anymore
+                        #print("outdated_batch", id_retrieved[num_l])
+                        id_to_remove.append(num_l)
+                        continue
+
+                    length = int(length)
+                    err_list = errors[last_l:last_l + length]
                     err = self.prioritize_max_mean_balance * np.max(err_list) + (
                                 1 - self.prioritize_max_mean_balance) * np.mean(err_list)
                     processed_errors.append(err)
-                    last_l += l
+                    last_l += length
+
+                id_list = id_retrieved.tolist()
+                for rm_id in reversed(id_to_remove):
+                    id_list.pop(rm_id)
+
+                id_retrieved = id_list
+                if len(id_retrieved) == 1:
+                    processed_errors = processed_errors[0]
+                elif len(id_list) == 0:
+                    return
             else:
                 delta = errors + self.prioritize_eps
                 processed_errors = np.power(delta, self.prioritize_alpha)
 
             self.batch_sampler.update_transitions_proba(processed_errors, id_retrieved)
 
-    def compute_is_weights(self, id_retrieved):
+    def compute_is_weights(self, id_retrieved, verification_id, batch_length):
         if self.use_prioritization:
             is_weights = self.batch_sampler.compute_is_weights(id_retrieved)
             if self.is_recurrent:
                 expand_is_weight_list = []
-                for num_weight, length in enumerate(self.dataset.episode_length[id_retrieved]):
+                for num_weight, length in enumerate(batch_length):
+                    if self.dataset.episode_identificator[id_retrieved[num_weight]] != verification_id[num_weight]:
+                        print("Outdated IS : id batch {}, id memory  {}".format(
+                            verification_id[num_weight],
+                            self.dataset.episode_identificator[id_retrieved][num_weight]))
+                        current_is_weight = min(is_weights)
+                    else:
+                        current_is_weight = is_weights[num_weight]
+
                     temp_w = np.empty(int(length))
-                    temp_w[:] = is_weights[num_weight]
+                    temp_w[:] = current_is_weight
                     expand_is_weight_list.append(temp_w)
 
                 is_weights = np.concatenate(expand_is_weight_list)

src/algo/replay_buffer_tools.py
@@ -5,23 +5,6 @@ from torch.utils.data import Dataset, DataLoader, Sampler
 
 from algo.compression import BloscArrayCompressor
 
-def sample(memory, n_memory_cell, transition_proba, batch_size, compressor):
-
-    batch_seq = []
-    batch_size = batch_size // 10
-
-    id_taken = list()
-
-    for i in range(batch_size):
-        id_episode = np.random.choice(range(n_memory_cell), p=transition_proba)
-        seq = [compressor.decompress_transition(transition) for transition in memory[id_episode]]
-        batch_seq.append(seq)
-        id_taken.append(id_episode)
-
-    last_id_sampled = np.array(id_taken)
-
-    return batch_seq, last_id_sampled
-
 class MissionPadder(object):
     def __init__(self, max_length, padding_token):
         self.max_length = max_length
@@ -70,15 +53,13 @@ class WeightedSampler(Sampler):
         self.prioritize_p[id_retrieved] = errors
 
     def compute_is_weights(self, id_retrieved):
-        min_proba = np.min(self.prioritize_proba[:len(self)])
+        prioritize_proba = self.prioritize_p[:self.n_memory_cell] / self.prioritize_p[:self.n_memory_cell].sum()
+        min_proba = np.min(prioritize_proba)
         max_w = (len(self) * min_proba) ** - self.prioritize_beta
-        is_weights = np.power(self.prioritize_proba[id_retrieved] * len(self), - self.prioritize_beta)
+        is_weights = np.power(prioritize_proba[id_retrieved] * len(self), - self.prioritize_beta)
         is_weights /= max_w
 
         return is_weights
-
-    def compute_prioritize_proba(self):
-        self.prioritize_proba = self.prioritize_p[:self.n_memory_cell] / self.prioritize_p[:self.n_memory_cell].sum()
 
     def __len__(self):
         return self.n_memory_cell # Number of cell used the replay buffer
@@ -87,9 +68,10 @@ class WeightedSampler(Sampler):
         n_max_sampling = self.n_memory_cell
         if self.use_prioritization:
             while True:
+                prioritize_proba = self.prioritize_p[:n_max_sampling] / self.prioritize_p[:n_max_sampling].sum()
                 yield np.random.choice(self.id_range[:n_max_sampling],
                                        size=self.batch_size,
-                                       p=self.prioritize_proba[n_max_sampling])
+                                       p=prioritize_proba[:n_max_sampling])
         else:
             while True:
                 yield np.random.choice(self.id_range[:n_max_sampling],
@@ -153,6 +135,9 @@ class MemoryDatasetRecurrent(MemoryDataset):
         # Override to save some space
         self.memory = [None for _ in range(memory_allocated)]
         self.episode_length = np.zeros(memory_allocated)
+        self.episode_identificator = np.zeros(memory_allocated)
+
+        self.id_current_episode = 0
 
         self.max_length_seq = max_length_seq
         self.len_sum = 0
@@ -184,6 +169,8 @@ class MemoryDatasetRecurrent(MemoryDataset):
         # Store episode and length
         self.memory[self.position] = episode_to_store
         self.episode_length[self.position] = len_episode
+        self.episode_identificator[self.position] = self.id_current_episode
+        self.id_current_episode += 1
 
         # Push selector and update buffer length if necessary
         self.position += 1
@@ -207,12 +194,14 @@ class MemoryDatasetRecurrent(MemoryDataset):
             "action": [],
             "reward": [],
             "gamma": [],
-            "id_retrieved" : [id_episode]
+            "id_retrieved" : [id_episode],
+            "id_verification" : [self.episode_identificator[id_episode]]
         }
 
         sequence = self.memory[id_episode]
         #print(id_episode, self.n_memory_cell)
         len_sequence = len(sequence)
+        assert self.episode_length[id_episode] == len_sequence
 
         for transition in sequence:
 

src/logging_helper.py
@@ -3,6 +3,8 @@ import numpy as np
 import json
 from os import path
 
+import matplotlib.pyplot as plt
+
 class SweetLogger(SummaryWriter):
     def __init__(self, dump_step, path_to_log=None):
         """
@@ -41,7 +43,7 @@ class SweetLogger(SummaryWriter):
         # Each contains a list of values, and operation(s?) you want to apply
         self.variable_to_log = dict()
         self.sentence_to_log = dict()
-        self.buffer_id_to_log = dict()
+        self.buffer_id_log = dict()
 
         self.dump_step = dump_step
         self.str2op = {'mean': np.mean, 'max': np.max, 'min': np.min}
@@ -71,7 +73,7 @@ class SweetLogger(SummaryWriter):
             # Dump buffer id
             id_file_name = "id_count_step{}.json".format('train' if train else 'test')
             id_log_path = path.join(self.path_to_log, id_file_name)
-            json.dump(self.buffer_id_to_log, fp=open(id_log_path, 'w'), indent='    ', separators=('', ':'))
+            json.dump(self.buffer_id_log, fp=open(id_log_path, 'w'), indent='    ', separators=('', ':'))
 
             # Dump variables
             for variable_name, var_dict in self.variable_to_log.items():
@@ -98,13 +100,23 @@ class SweetLogger(SummaryWriter):
     def add_image(self, tag, img_tensor, global_step=None, walltime=None, dataformats='CHW'):
         super().add_image(tag, img_tensor, global_step, walltime, dataformats)
 
+    # def dump_buffer_id_hist(self):
+    #
+    #     buffer_id_log_array = np.zeros(int(max(self.buffer_id_log)))
+    #     for key in self.buffer_id_log:
+    #         key
+    #
+    #     bins = []
+    #     for i in range(len(a) // 100):
+    #         bins.append(a[i * 100:(i + 1) * 100].sum())
+
     def store_buffer_id(self, ids):
         for id in ids:
             id = str(id)
-            if id in self.buffer_id_to_log:
-                self.buffer_id_to_log[id] += 1
+            if id in self.buffer_id_log:
+                self.buffer_id_log[id] += 1
             else:
-                self.buffer_id_to_log[id] = 1
+                self.buffer_id_log[id] = 1
 
     def store_sentences(self, sentence, reward):
 